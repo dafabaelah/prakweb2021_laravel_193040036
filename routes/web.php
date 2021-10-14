6553e3981a2499630c545529ca0d6b8152e79e98
dafabaelah <dafabaelah12@gmail.com>
2021-10-14 08:51:23 +0700

    Belajar Laravel 8 | 5. Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,55 +33,7 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Sandhika Galih",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aliquid consectetur temporibus. Facilis similique ut nostrum quod rerum. Exercitationem officia saepe labore omnis ut eligendi aspernatur dignissimos ipsum blanditiis enim."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Doddy Ferdiansyah",
-            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam temporibus, nesciunt perferendis quidem accusamus dicta dolores voluptas distinctio dignissimos suscipit! Inventore explicabo modi quidem maiores hic voluptatem accusamus, debitis porro quaerat temporibus veniam suscipit blanditiis corporis alias minus amet sapiente assumenda similique voluptatum, odio numquam perspiciatis? Voluptate in nam reprehenderit. Minus, vitae, enim dignissimos soluta cumque esse suscipit labore, iste ut ab animi quo? Debitis maiores tenetur deserunt aliquam alias magnam expedita aut laudantium vel repellendus? Autem provident qui laborum repellendus a, ducimus cumque nisi totam quae, corrupti quod? Vel repudiandae ratione laudantium reprehenderit ab aut assumenda non hic molestiae."
-        ]
-        ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-    
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // halman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Sandhika Galih",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aliquid consectetur temporibus. Facilis similique ut nostrum quod rerum. Exercitationem officia saepe labore omnis ut eligendi aspernatur dignissimos ipsum blanditiis enim."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Doddy Ferdiansyah",
-            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam temporibus, nesciunt perferendis quidem accusamus dicta dolores voluptas distinctio dignissimos suscipit! Inventore explicabo modi quidem maiores hic voluptatem accusamus, debitis porro quaerat temporibus veniam suscipit blanditiis corporis alias minus amet sapiente assumenda similique voluptatum, odio numquam perspiciatis? Voluptate in nam reprehenderit. Minus, vitae, enim dignissimos soluta cumque esse suscipit labore, iste ut ab animi quo? Debitis maiores tenetur deserunt aliquam alias magnam expedita aut laudantium vel repellendus? Autem provident qui laborum repellendus a, ducimus cumque nisi totam quae, corrupti quod? Vel repudiandae ratione laudantium reprehenderit ab aut assumenda non hic molestiae."
-        ]
-        ];
-
-        $new_post = [];
-        foreach($blog_posts as $post) {
-            if($post["slug"] === $slug) {
-                $new_post = $post;
-
-            }
-        }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
