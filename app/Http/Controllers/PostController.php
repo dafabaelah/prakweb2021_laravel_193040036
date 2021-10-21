9c1742d3a536db1085ca90c54d80859a5900b0a0
dafabaelah <dafabaelah12@gmail.com>
2021-10-21 10:55:44 +0700

    Belajar Laravel 8 | 11. N+1 Problem

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 class PostController extends Controller {
     public function index() {
         return view('posts', [
-            "title" => "Posts",
+            "title" => "All Posts",
             // "posts" => Post::all()
-            "posts" => Post::latest()->get()
+            "posts" => Post::with(['author', 'category'])->latest()->get()
             
         ]);
     }
