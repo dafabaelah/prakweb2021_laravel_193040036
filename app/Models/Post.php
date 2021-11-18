cddfdda96aec96e5d2a4457561918cf093b6ea98
dafabaelah <dafabaelah12@gmail.com>
2021-11-18 19:46:55 +0700

    Belajar Laravel 8 | 18. Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
@@ -45,5 +48,14 @@ class Post extends Model
     {
         return 'slug';
     }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    } 
 
 }
