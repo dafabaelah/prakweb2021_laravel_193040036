85309b58c925d02cfa9c09cdddf8ec1c7b9a8cc3
dafabaelah <dafabaelah12@gmail.com>
2021-11-25 16:54:49 +0600

    Belajar Laravel 8 | 23. Authorization

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -103,14 +104,22 @@ class DashboardPostController extends Controller
             'title' => 'required|max:255',
             // 'slug' => 'required|unique:posts'
             'category_id' => 'required',
+            'image' => 'image|file|max:50000',
             'body' => 'required'
         ]);
+
 
         if($request->slug != $post->slug) {
             $rules['slug'] = 'required|unique:posts';
         }
 
         $validateData = $request->validate($rules);
+        if($request->file('image')) {
+            if($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validateData['image'] = $request->file('image')->store('post-images');
+        }
         Post::where('id', $post->id)
                 ->update($validateData);
 
@@ -125,6 +134,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
